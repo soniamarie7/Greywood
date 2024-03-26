c93760802bfaa834b3d9ba833793123492783b2a
sonialinse7 <sonialinse7@gmail.com>
2024-03-26 18:04:42 -0400

    updating project structure

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,20 @@
-package src;
-
 public class Player {
+        private String name;
+        private int healthPoints;
+        private int courage;
+        private int stamina;
+        private int magic;
+        private int experienceLevel;
+    
+        // Constructor
+        public Player() {
+            this.healthPoints = 100;
+            this.courage = 11;
+            this.stamina = 100;
+            this.magic = 15;
+            this.experienceLevel = 5;
+         }
+ }
+    
+        // Getters and setters...
 
-}
